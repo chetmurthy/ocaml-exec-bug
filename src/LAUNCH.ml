1d1f353be7574c06becf9591720126fe2fa06568
Chet Murthy <chetsky@gmail.com>
2024-12-15 17:37:57 -0800

    add "very verbose" flag to print out command + env vars

--- src/LAUNCH.ml
+++ src/LAUNCH.ml
@@ -6,11 +6,13 @@ open Fpath
 let push l x = l := x :: !l
 
 let verbose = ref false
+let veryverbose = ref false
 let cmd = ref []
 let _ =
   Arg.
   (parse
     ["-v", Set verbose, "verbose output";
+     "-vv", Set veryverbose, "very verbose output";
      "--", Rest (fun s -> cmd := !cmd @ [s]), "the command"]
     (fun s -> cmd := !cmd @ [s]) "LAUNCH [-v] [--] <cmd>")
 
@@ -26,20 +28,26 @@ let main () =
              "LAUNCH: environment variable TOP *must* be set to use this wrapper")
   in
   let* path = OS.Env.req_var "PATH" in
+  let newpath = String.concat "" [top; "/local-install/bin:"; path] in
   let* () =
-    OS.Env.set_var "PATH"
-      (Some (String.concat "" [top; "/local-install/bin:"; path]))
+    OS.Env.set_var "PATH" (Some newpath)
   in
+  let newcamlpath = String.concat "" [top; "/local-install/lib:"] in
   let* () =
-    OS.Env.set_var "OCAMLPATH"
-      (Some (String.concat "" [top; "/local-install/lib:"]))
+    OS.Env.set_var "OCAMLPATH" (Some newcamlpath)
   in
   match !cmd with
     exe :: _ ->
       if !verbose then
         Fmt.
         (pf stderr "LAUNCH: command %a\n%!"
-          (list ~sep:(const string " ") Dump.string) !cmd);
+           (list ~sep:(const string " ") Dump.string) !cmd);
+      if !veryverbose then
+        Fmt.
+        (pf stderr "LAUNCH: env PATH=%a OCAMLPATH=%a %a\n%!"
+           Dump.string newpath
+           Dump.string newcamlpath
+           (list ~sep:(const string " ") Dump.string) !cmd);
       Ok (Unix.execvp exe (Array.of_list !cmd))
   | _ ->
       Error
