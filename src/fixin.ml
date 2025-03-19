5b9a2efa5ad9426adfa8e0f2cc68f1a372c1f203
Tobiasz Laskowski <tobil4sk@outlook.com>
2025-03-19 20:25:43 +0000

    Fix PATH variable separator on windows

--- src/fixin.ml
+++ src/fixin.ml
@@ -19,8 +19,12 @@ let _ =
   (parse ["-s", Arg.Clear verbose, "silence verbosity"]
     (fun s -> push files s) "fixin [-s] <files>")
 
+let path_var_separator = match Sys.os_type with
+  | "Unix" -> ':'
+  | _ -> ';'
+
 let search_path =
-  let dirs = String.split_on_char ':' (Sys.getenv "PATH") in
+  let dirs = String.split_on_char path_var_separator (Sys.getenv "PATH") in
   List.map Fpath.v dirs
 
 let fix_interpreter ~f (exedir, exename) =
