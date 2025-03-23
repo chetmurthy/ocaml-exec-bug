8ee4b3e51502449cbf7f112a331e03da702b1e85
Chet Murthy <chetsky@gmail.com>
2025-03-22 20:04:39 -0700

    meh

---
+++ test/simple/LAUNCH.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "================"
+../../src/LAUNCH echo foo
+
+echo "================"
+env TOP=../.. ../../src/LAUNCH echo foo
+
+echo "================"
+env TOP=../.. ../../src/LAUNCH -- ocamlfind camlp5-buildscripts/LAUNCH -- echo bar
